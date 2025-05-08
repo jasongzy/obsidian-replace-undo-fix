05178ec4936ef4aaec138770d7c868e7311ecfd4
Zhiyang Guo <jasongzy@gmail.com>
2025-05-08 23:15:07 +0800

    🎉 init

--- main.ts
+++ main.ts
@@ -1,134 +1,51 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { EditorSelection, Transaction, TransactionSpec } from '@codemirror/state';
+import { EditorView } from '@codemirror/view';
+import { Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+// Regular expression to detect common full-width characters.
+// Includes full-width ASCII, CJK symbols, and punctuation.
+const FULL_WIDTH_CHARS_REGEX = /[\uFF01-\uFF5E\u3000-\u303F]/;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+export default class FullWidthUndoFixPlugin extends Plugin {
+    async onload() {
+        // console.log('Loading Replace Undo Fix Plugin');
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+        this.registerEditorExtension(EditorView.inputHandler.of(
+            (view: EditorView, from: number, to: number, text: string, _insert: () => Transaction): boolean => {
+                // Check if:
+                // 1. A single character is being inserted.
+                // 2. The character is a full-width character.
+                // 3. There is an active selection being replaced (from !== to).
+                const isSingleChar = text.length === 1;
+                const isFullWidthChar = FULL_WIDTH_CHARS_REGEX.test(text);
+                const isReplacingSelection = from !== to;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+                if (isSingleChar && isFullWidthChar && isReplacingSelection) {
+                    // console.log(`InputHandler: Detected full-width char [${text}] replacing selection from ${from} to ${to}.`);
 
-	async onload() {
-		await this.loadSettings();
+                    const transactionSpec: TransactionSpec = {
+                        changes: { from, to, insert: text },
+                        selection: EditorSelection.cursor(from + text.length), // Place cursor after the inserted character
+                        userEvent: 'input.replace', // Mark as a user-initiated replacement for undo history
+                        scrollIntoView: true
+                    };
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+                    view.dispatch(view.state.update(transactionSpec));
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+                    // Return true to indicate that this input has been handled,
+                    // preventing default behavior and other handlers.
+                    return true;
+                }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+                // If conditions are not met, let other handlers or default behavior process the input.
+                return false;
+            }
+        ));
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+        // console.log('Replace Undo Fix Plugin loaded');
+    }
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    onunload() {
+        // console.log('Unloading Replace Undo Fix Plugin');
+    }
 }
